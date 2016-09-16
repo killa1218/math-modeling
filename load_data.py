dd547401f231b57589fe50b6b6220c533af9b139
ccyyatnet <1124015396@qq.com>
2016-09-16 14:37:27 +0800

    fix and test

--- load_data.py
+++ load_data.py
@@ -3,11 +3,11 @@ import numpy as np
 class DataLoader():
 	def __init__(self, folder='data/'):
 		self._folder = folder
-		print 'Init DataLoader.'
+		print 'Init DataLoader Done.'
 
 	def load_genotype(self):
 		with open('data/genotype.dat', 'r') as infile:
-			lines = f.readlines()
+			lines = infile.readlines()
 		genotype = lines[0].split()
 		genotable = []
 		for line in lines[1:]:
@@ -27,3 +27,19 @@ class DataLoader():
 				gene_info.append(infile.read().split())
 		return gene_info
 
+if __name__=='__main__':
+	print 'Testing DataLoader...'
+	data_loader = DataLoader()
+	
+	genotype, genotable = data_loader.load_genotype() 
+	print 'genotype:\n', '\tshape:', len(genotype), 'sample:', genotype[0]
+	print 'genotable:\n', '\tshape:', len(genotable), len(genotable[0]), 'sample:', genotable[0][0]
+
+	phenotype = data_loader.load_phenotype()
+	print 'phenotype:\n', '\tshape:', phenotype.shape, 'sample:', phenotype[0]
+
+	multi_phenos = data_loader.load_multi_phenos()
+	print 'multi_phenos:\n', '\tshape:', multi_phenos.shape, 'sample:', multi_phenos[0]
+
+	gene_info = data_loader.load_gene_info()
+	print 'gene_info:\n', '\tshape:', len(gene_info), 'sample:', gene_info[0]
